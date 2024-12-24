110e77541bbe02f56e10c08f5df97008b6358db1
SebastianFajardoSoch <sfajardo@qvision.com.co>
2024-12-24 09:31:09 -0500

    Ajustes en valor de constante Api

--- src/Presentation/constants/index.tsx
+++ src/Presentation/constants/index.tsx
@@ -1,3 +1,3 @@
 export const API_PORTAL = "https://tradego-suite-portal-uat.altipal.com.co/api";
-export const API = "http://localhost:3100/api";
+export const API = "https://tradego-suite-backend-uat.altipal.com.co/api";
 export const urlBasePing = `${API}/OrderNoSales/1`;
